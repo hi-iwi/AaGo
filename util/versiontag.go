ca4503af76329be709d127fdd0b0fd0f9cd29ffa
Aario <Aario@luexu.com>
2019-04-03 16:56:36 +0800

    read 40 characters from git commit log

--- util/versiontag.go
+++ util/versiontag.go
@@ -10,15 +10,12 @@ var gitHash string
 func GitVersion() string {
 	if gitHash == "" {
 		s, _ := exec.LookPath(os.Args[0])
-
 		file, err := os.OpenFile(s, os.O_RDONLY, 0666)
 		if err == nil {
 			defer file.Close()
-
-			fInfo, _ := file.Stat()
-
+			finfo, _ := file.Stat()
 			hash := make([]byte, 40)
-			file.ReadAt(hash, fInfo.Size()-40)
+			file.ReadAt(hash, finfo.Size()-40-1)
 			gitHash = string(hash)
 		}
 	}
