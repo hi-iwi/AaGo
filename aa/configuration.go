341ccf8b10f1aebfff207949efd305ddf0fd91d8
Aario@luexu.com <Aario@luexu.com>
2019-04-04 18:27:21 +0800

    mysql and amqp healthcheck

--- aa/configuration.go
+++ aa/configuration.go
@@ -1,8 +1,8 @@
 package aa
 
 type Configuration struct {
-	Service    string
-	ServiceID  string
-	Env        string // dev test preprod product
-	TimezoneID string // e.g. Asia/Shanghai
+	Service    string `yaml:"service"`
+	ServerID   string `yaml:"server_id"`
+	Env        string `yaml:"env"`         // dev test preprod product
+	TimezoneID string `yaml:"timezone_id"` // e.g. Asia/Shanghai
 }
