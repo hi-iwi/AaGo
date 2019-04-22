ad0da80e7d8cfe40bd1a3f501d3c0ac4c1ba670b
Aario <Aario@luexu.com>
2019-04-22 16:54:53 +0800

    sql errors

--- aa/error.go
+++ aa/error.go
@@ -2,6 +2,7 @@ package aa
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"fmt"
 
 	"github.com/luexu/AaGo/aa"
@@ -32,12 +33,31 @@ func NewError(code int, msg ...interface{}) *Error {
 }
 
 func NewSqlError(err error) *Error {
-	if err == sql.ErrNoRows {
-		return aa.NewError(404, fmt.Sprintf("sql query not found: ", err))
-	} else if err != nil {
-		return aa.NewError(500, fmt.Sprintf("sql query error: ", err))
+	if err == nil {
+		return nil
 	}
-	return nil
+	switch err {
+	case driver.ErrBadConn:
+		aa.NewError(500, fmt.Sprintf("sql bad conn: ", err))
+	case driver.ErrSkip:
+		// ErrSkip may be returned by some optional interfaces' methods to
+		// indicate at runtime that the fast path is unavailable and the sql
+		// package should continue as if the optional interface was not
+		// implemented. ErrSkip is only supported where explicitly
+		// documented.
+		aa.NewError(500, fmt.Sprintf("sql skip: ", err))
+	case driver.ErrRemoveArgument:
+		aa.NewError(500, fmt.Sprintf("sql remove argument: ", err))
+	case sql.ErrConnDone:
+		// ErrConnDone is returned by any operation that is performed on a connection
+		// that has already been returned to the connection pool.
+		aa.NewError(500, fmt.Sprintf("sql conn done: ", err))
+	case sql.ErrTxDone:
+		return aa.NewError(500, fmt.Sprintf("sql tx done: ", err))
+	case sql.ErrNoRows:
+		return aa.NewError(404, fmt.Sprintf("sql query not found: ", err))
+	}
+	return aa.NewError(500, err)
 }
 
 func (e *Error) Error() string {
