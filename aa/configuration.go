672c495ca1246ce4c5d638b6b463e231ef09cf41
Aario <Aario@luexu.com>
2019-04-23 16:42:51 +0800

    add configuration mock

--- aa/configuration.go
+++ aa/configuration.go
@@ -5,4 +5,5 @@ type Configuration struct {
 	ServerID   string `yaml:"server_id"`
 	Env        string `yaml:"env"`         // dev test preprod product
 	TimezoneID string `yaml:"timezone_id"` // e.g. Asia/Shanghai
+	Mock       bool   // using mock
 }
