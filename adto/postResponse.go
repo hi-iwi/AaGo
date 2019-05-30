7645e9a63fcc32f73e59fff3296d6913b03d447d
Aario <Aario@luexu.com>
2019-05-30 11:47:38 +0800

    add count resp adto

--- adto/postResponse.go
+++ adto/postResponse.go
@@ -3,3 +3,7 @@ package adto
 func PostResp(id interface{}) map[string]interface{} {
 	return map[string]interface{}{"id": id}
 }
+
+func CountResp(cnt interface{}) map[string]interface{} {
+	return map[string]interface{}{"total": cnt}
+}
