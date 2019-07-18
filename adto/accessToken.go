afcebc37ce79f5e03bbfafee82a9be7bf89f3a18
Aario <Aario@luexu.com>
2019-07-18 18:05:22 +0800

    access token

--- adto/accessToken.go
+++ adto/accessToken.go
@@ -4,6 +4,6 @@ type AccessToken struct {
 	AccessToken  string `json:"access_token"`
 	ExpiresIn    int64  `json:"expires_in"` // expires in seconds
 	RefreshToken string `json:"refresh_token"`
-	// TokenType string `json:"token_type"`
-	// Scope     string `json:"scope"`
+	TokenType    string `json:"token_type"` // OAuth 2.0 / Bearer
+	Scopes       string `json:"scopes"`
 }
