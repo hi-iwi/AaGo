b74f18ea8d3f427d59bf2990fc3c4c72c2ae92e9
Aario <Aario@luexu.com>
2019-07-19 12:51:48 +0800

    cocontext

--- ae/redis.go
+++ ae/redis.go
@@ -5,8 +5,8 @@ func NewRedisError(err error) *Error {
 		return nil
 	}
 
-	switch err {
-
+	if err.Error() == "redigo: nil returned" {
+		return NewError(404, "redis not found")
 	}
 
 	return NewError(500, "redis error: "+err.Error())
---
+++ util/context.go
@@ -0,0 +1,45 @@
+package util
+
+import (
+	"context"
+
+	"github.com/kataras/iris"
+	"github.com/luexu/alog"
+	"github.com/luexu/randm"
+)
+
+// CoContext a new context for coroutine
+func CoContext(ictx iris.Context) context.Context {
+	// @warn it must to be a child of context.Background(). Do not assign it to the child of iris.Context!
+	ctx := context.WithValue(context.Background(), alog.TraceID, ictx.Values().GetString(string(alog.TraceID))+"@co")
+	return ctx
+}
+
+func CoContextWithCancel(ictx iris.Context) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	// @warn it must to be a child of context.Background(). Do not assign it to the child of iris.Context!
+	ctx = context.WithValue(ctx, alog.TraceID, ictx.Values().GetString(string(alog.TraceID))+"@co")
+	return ctx, cancel
+}
+
+func traceID(cs ...context.Context) string {
+	tid := ""
+	if len(cs) > 0 {
+		c := cs[0]
+		tid, _ = c.Value(alog.TraceID).(string)
+	}
+	if tid == "" {
+		tid = randm.NewUUID().String()
+	}
+	return tid
+}
+func Ctx(cs ...context.Context) context.Context {
+	ctx := context.WithValue(context.Background(), alog.TraceID, traceID(cs...)+"@co")
+	return ctx
+}
+
+func CtxWithCancel(cs ...context.Context) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx = context.WithValue(ctx, alog.TraceID, traceID(cs...)+"@co")
+	return ctx, cancel
+}
