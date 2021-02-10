cb1099e954a07cf51f04a27e04f6cca87a8b0d44
Aario@luexu.com <Aario@luexu.com>
2021-02-11 01:14:16 +0800

    'NO_COMMENT'

--- upgrade_mod.sh
+++ upgrade_mod.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 # 更新 Go mod
+
+readonly root='/data/Aa/proj/go/src/github.com/hi-iwi'
+readonly comment="${1:-'NO_COMMENT'}"
+pushAndUpgradeMod(){
+  cd $root'/'$1 || exit
+  go get -u
+  git add -A .
+  git commit -m "$comment"
+  git push origin master
+}
+
+
+pushAndUpgradeMod 'aenum'
+pushAndUpgradeMod 'code'
+pushAndUpgradeMod 'dtype'
+pushAndUpgradeMod 'AaGo'  # 依赖 dtype
+pushAndUpgradeMod 'aorm'  # 依赖 AaGo
+
+
