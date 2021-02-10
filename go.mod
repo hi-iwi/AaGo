3b30bbd951833ee601e50f7905e67b8b6b2bc95a
Aario@luexu.com <Aario@luexu.com>
2021-02-11 01:37:51 +0800

    upgrade mod

--- go.mod
+++
@@ -1,8 +0,0 @@
-go 1.15
-
-require (
-	github.com/kataras/iris/v12 v12.1.8
-	github.com/google/uuid v1.2.0
-)
-
-module github.com/hi-iwi/AaGo
--- upgrade_mod.sh
+++ upgrade_mod.sh
@@ -8,12 +8,19 @@ pushAndUpgradeMod(){
   echo "upgrading $1 ..."
   cd "$root/$1" || exit
 
-  sed -i '/github\.com\/hi-iwi\/aenum/d' go.mod
-  sed -i '/github\.com\/hi-iwi\/code/d' go.mod
-  sed -i '/github\.com\/hi-iwi\/dtype/d' go.mod
-  sed -i '/github\.com\/hi-iwi\/AaGo/d' go.mod
-  sed -i '/github\.com\/hi-iwi\/aorm/d' go.mod
+#  sed -i '/github\.com\/hi-iwi\/aenum/d' go.mod
+#  sed -i '/github\.com\/hi-iwi\/aenum/d' go.sum
+#  sed -i '/github\.com\/hi-iwi\/code/d' go.mod
+#  sed -i '/github\.com\/hi-iwi\/code/d' go.sum
+#  sed -i '/github\.com\/hi-iwi\/dtype/d' go.mod
+#  sed -i '/github\.com\/hi-iwi\/dtype/d' go.sum
+#  sed -i '/github\.com\/hi-iwi\/AaGo/d' go.mod
+#  sed -i '/github\.com\/hi-iwi\/AaGo/d' go.sum
+#  sed -i '/github\.com\/hi-iwi\/aorm/d' go.mod
+#  sed -i '/github\.com\/hi-iwi\/aorm/d' go.sum
 
+  rm -f go.mod go.sum
+  
   go get -u
   git add -A .
   git commit -m "$comment"
