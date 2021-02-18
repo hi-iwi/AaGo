630c3c0680097819ad7d62fee3a6ee5e023ace8b
Aario@luexu.com <Aario@luexu.com>
2021-02-18 11:58:39 +0800

    fixing util err

--- upgrade_mod.sh
+++ upgrade_mod.sh
@@ -4,25 +4,39 @@
 
 readonly root='/data/Aa/proj/go/src/github.com/hi-iwi'
 readonly comment="${1:-'NO_COMMENT'}"
+upgrade=0
+
+
+while getopts "u" opt
+do
+  # shellcheck disable=SC2220
+  case $opt in
+  u)
+    echo "upgrading go.mod..."
+    upgrade=1
+  ;;
+  esac
+done
+
+
 pushAndUpgradeMod(){
   echo "upgrading $1 ..."
   cd "$root/$1" || exit
 
-
-  # 暂时用不到了
-  # 私有库问题
-  #rm -f go.mod
-  #go mod init
-  #env GIT_TERMINAL_PROMPT=1 go get -insecure github.com/hi-iwi/AaGo
-  #go get -u ./...
-
+  if [ $upgrade -eq 1 ]; then
+    rm -f go.mod
+    go mod init
+      # 私有库问题
+    env GIT_TERMINAL_PROMPT=1 go get -insecure github.com/hi-iwi/AaGo
+    go get -u ./...
+  fi
   git add -A .
   git commit -m "$comment"
   git push origin master
 }
 
 
-pushAndUpgradeMod 'AaGo'  # 依赖 dtype
+pushAndUpgradeMod 'AaGo'
 
 
 
