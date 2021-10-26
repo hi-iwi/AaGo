7c34b8f72ccf827cb71e041f32406e2d1411d582
Aario <Aario@luexu.com>
2021-10-26 22:49:53 +0800

    change eduction level

--- aenum/education.go
+++ aenum/education.go
@@ -1,20 +1,24 @@
 package aenum
 
-type EducationDegree uint8
-
+type EduLevel uint8 // 学历
+type EduConclusion uint8  // 毕结业结论
 const (
-	NoListedEducationDegree EducationDegree = 0
-	BelowHighSchool         EducationDegree = 1
-	HighSchoolDiploma       EducationDegree = 2 // 高中学历
+	GraduationConclusion EduConclusion = 1 // 毕业
 
-	ColleageDegree         EducationDegree = 50 // 专科
-	FullTimeColleageDegree EducationDegree = 51 // 全日制专科
+)
+const (
+	NoEduLevel        EduLevel = 0
+	BelowHighSchool   EduLevel = 1
+	HighSchoolDiploma EduLevel = 2 // 高中学历
 
-	BachelorDegree         EducationDegree = 80 // 本科
-	FullTimeBachelorDegree EducationDegree = 81 // 全日制本科
-	EliteBachelorDegree    EducationDegree = 82 // 全日制211/985院校，精英院校
+	ColleageDegree         EduLevel = 50 // 专科
+	FullTimeColleageDegree EduLevel = 51 // 全日制专科
 
-	PostgraduateDegree EducationDegree = 100 // 研究生
-	DoctorateDegree    EducationDegree = 101 // 博士
-	Postdoctoral       EducationDegree = 102 // 博士后
+	BachelorDegree         EduLevel = 80 // 本科
+	FullTimeBachelorDegree EduLevel = 81 // 全日制本科
+	EliteBachelorDegree    EduLevel = 82 // 全日制211/985院校，精英院校
+
+	PostgraduateDegree EduLevel = 100 // 研究生
+	DoctorateDegree    EduLevel = 101 // 博士
+	Postdoctoral       EduLevel = 102 // 博士后
 )
