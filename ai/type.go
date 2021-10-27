eac2d451444844787bca0ac7317491a0e845b05d
Aario@luexu.com <Aario@luexu.com>
2021-10-28 04:15:11 +0800

    add ai.Uint64Str

--- ai/type.go
+++ ai/type.go
@@ -1,3 +1,8 @@
 package ai
 
-type Uint64Str string // 数据存的是 uint64，拿取到接口都是 string
+type (
+	Uint64Str string // ID 数据存的是 uint64，拿取到接口都是 string
+	Uint32Str string
+	Uint16Str string
+	Uint8Str  string
+)
