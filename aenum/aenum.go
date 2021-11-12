55d701a897091f3aba28b2bbb0f124387861ad1c
Aario@luexu.com <Aario@luexu.com>
2021-11-12 17:08:34 +0800

    remove string chan

--- aenum/aenum.go
+++ aenum/aenum.go
@@ -2,8 +2,10 @@ package aenum
 
 // func NewDemo() (Demo, bool){}
 type Demo interface {
+	Raw()  interface{}           // 原始值，如 uint16、byte、string等
 	Valid() bool                  // 是否在正确范围内
 	String() string               // 类型转换为string
 	Name() string                 // 显示给客户端，同时为了避免客户端侦测太多服务端信息，特别是作为加密因子
+	Is(demo Demo) bool // 判断是否相等
 	In(series []interface{}) bool // 是否在某个系列里面
 }
