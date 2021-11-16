76247f485becf7daf4c7968c6798f45e202d3193
Aario@luexu.com <Aario@luexu.com>
2021-11-17 05:16:49 +0800

    fast config demo

--- aa/fast_config_demo.go
+++ aa/fast_config_demo.go
@@ -16,6 +16,11 @@ type MysqlConfig struct {
 	ConnTimeout  time.Duration // 使用时，需要设置单位，s, ms等。Timeout for establishing connections, aka dial timeout
 	ReadTimeout  time.Duration // 使用时，需要设置单位，s, ms等。I/O read timeout.
 	WriteTimeout time.Duration // 使用时，需要设置单位，s, ms等。I/O write timeout.
+
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
+	ConnMaxIdleTime time.Duration
 }
 
 func (app *Aa) MysqlConfig(section string) (MysqlConfig, error) {
@@ -38,19 +43,48 @@ func (app *Aa) MysqlConfig(section string) (MysqlConfig, error) {
 	tls := app.Config.GetString(section+".tls", "false")
 	timeout := app.Config.GetString(section+".timeout", "10s,10s,10s")
 	ct, rt, wt := app.ParseTimeout(timeout)
+	maxIdleConns := app.Config.Get(section + ".max_idle_conns").DefaultInt(0)
+	maxOpenConns := app.Config.Get(section + ".max_open_conns").DefaultInt(0)
+	connMaxLifetime := app.Config.Get(section + ".conn_max_life_time").DefaultInt64(0)
+	connMaxIdleTime := app.Config.Get(section + ".conn_max_idle_time").DefaultInt64(0)
 	cf := MysqlConfig{
-		Schema:       schema,
-		User:         user,
-		Password:     password,
-		TLS:          tls,
-		Host:         host,
-		ConnTimeout:  ct,
-		ReadTimeout:  rt,
-		WriteTimeout: wt,
+		Schema:          schema,
+		User:            user,
+		Password:        password,
+		TLS:             tls,
+		Host:            host,
+		ConnTimeout:     ct,
+		ReadTimeout:     rt,
+		WriteTimeout:    wt,
+		MaxIdleConns:    maxIdleConns,
+		MaxOpenConns:    maxOpenConns,
+		ConnMaxLifetime: time.Duration(connMaxLifetime) * time.Second,
+		ConnMaxIdleTime: time.Duration(connMaxIdleTime) * time.Second,
 	}
 	return cf, nil
 }
 
+type RedisPoolConfig struct {
+	// Maximum number of idle connections in the pool.
+	MaxIdle int
+
+	// Maximum number of connections allocated by the pool at a given time.
+	// When zero, there is no limit on the number of connections in the pool.
+	MaxActive int
+
+	// Close connections after remaining idle for this duration. If the value
+	// is zero, then idle connections are not closed. Applications should set
+	// the timeout to a value less than the server's timeout.
+	IdleTimeout time.Duration
+
+	// If Wait is true and the pool is at the MaxActive limit, then Get() waits
+	// for a connection to be returned to the pool before returning.
+	Wait bool
+
+	// Close connections older than this duration. If the value is zero, then
+	// the pool does not close connections based on age.
+	MaxConnLifetime time.Duration
+}
 type RedisConfig struct {
 	TLS  bool
 	Host string
@@ -60,6 +94,7 @@ type RedisConfig struct {
 	ConnTimeout  time.Duration
 	ReadTimeout  time.Duration
 	WriteTimeout time.Duration
+	Pool         RedisPoolConfig
 }
 
 func (app *Aa) RedisConfig(section string) (RedisConfig, error) {
@@ -72,6 +107,12 @@ func (app *Aa) RedisConfig(section string) (RedisConfig, error) {
 	db, _ := app.Config.Get(section+".db", 0).Uint8()
 	timeout := app.Config.Get(section+".timeout", " 3s, 3s, 3s").String()
 	ct, rt, wt := app.ParseTimeout(timeout)
+
+	poolMaxIdle := app.Config.Get(section + ".pool_max_idle").DefaultInt(0)
+	poolMaxActive := app.Config.Get(section + ".pool_max_active").DefaultInt(0)
+	poolIdleTimeout := app.Config.Get(section + ".pool_idle_timeout").DefaultInt64(0)
+	poolWait := app.Config.Get(section + ".pool_wait").DefaultBool(false)
+	poolConnLifeTime := app.Config.Get(section + ".pool_conn_life_time").DefaultInt64(0)
 	cf := RedisConfig{
 		TLS:          tls,
 		Host:         host,
@@ -80,6 +121,13 @@ func (app *Aa) RedisConfig(section string) (RedisConfig, error) {
 		ConnTimeout:  ct,
 		ReadTimeout:  rt,
 		WriteTimeout: wt,
+		Pool: RedisPoolConfig{
+			MaxIdle:         poolMaxIdle,
+			MaxActive:       poolMaxActive,
+			IdleTimeout:     time.Duration(poolIdleTimeout) * time.Second,
+			Wait:            poolWait,
+			MaxConnLifetime: time.Duration(poolConnLifeTime) * time.Second,
+		},
 	}
 	return cf, nil
 }
