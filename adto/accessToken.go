caa187929f741f7deda0e8c31d29db9e0da486dd
Aario@luexu.com <Aario@luexu.com>
2021-11-23 21:17:50 +0800

    change file names

--- adto/accessToken.go
+++
@@ -1,9 +0,0 @@
-package adto
-
-type AccessToken struct {
-	AccessToken  string `json:"access_token"`
-	ExpiresIn    int64  `json:"expires_in"` // expires in seconds
-	RefreshToken string `json:"refresh_token"`
-	TokenType    string `json:"token_type"` // OAuth 2.0 / Bearer
-	Scopes       string `json:"scopes"`
-}
