8cfd17d991d592ea600a23c57b367336966b5a3d
Iwi <Aario@luexu.com>
2022-04-03 20:53:52 +0800

    remove dir/url field from adto.ImgSrc

--- adto/mimesrc.go
+++ adto/mimesrc.go
@@ -1,19 +1,12 @@
 package adto
 
-import (
-	"regexp"
-	"strconv"
-)
-
 // 存储在数据库里面，图片列表，为了节省空间，用数组来
 // 数据库存储方式为 dtype.NullImgSrc，即  [path,size,width,height]
 type ImgSrc struct {
 	Processor int    `name:"-" json:"processor"` // 图片处理ID，如阿里云图片处理、网易云图片处理等
 	Fill      string `name:"-" json:"fill"`      // e.g.  https://xxx/img.jpg?width=${WIDTH}&height=${HEIGHT}
 	Fit       string `name:"-" json:"fit"`       // e.g. https://xxx/img.jpg?maxwidth=${MAXWIDTH}
-	Url       string `name:"-" json:"url"`
-	Dir       string `name:"-" json:"dir"`
-	Path      string `name:"path" json:"path"`
+	Path      string `name:"path" json:"path"`   // path 可能是 filename，也可能是 带文件夹的文件名
 	Size      uint32 `name:"size" json:"size"`
 	Width     uint16 `name:"width" json:"width"`
 	Height    uint16 `name:"height" json:"height"`
@@ -22,41 +15,9 @@ type ImgSrc struct {
 type VideoSrc struct {
 	Processor int    `name:"-" json:"processor"`
 	Fit       string `name:"-" json:"fit"` // e.g.  https://xxx/video.avi?quality=${QUALITY}
-	Url       string `name:"-" json:"url"`
-	Dir       string `name:"-" json:"dir"`
 	Path      string `name:"path" json:"path"`
 	Size      uint32 `name:"size" json:"size"`
 	Width     uint16 `name:"width" json:"width"`
 	Height    uint16 `name:"height" json:"height"`
 	Duration  uint32 `name:"duration" json:"duration"` // 时长，秒
-}
-
-func EncodeImgSrc(content string) []ImgSrc {
-	reg, _ := regexp.Compile(`<img([^>]+)data-path="([^"]+)"([^>]*)>`)
-	dataReg, _ := regexp.Compile(`data-([a-z]+)="([^"]+)"`)
-	matches := reg.FindAllStringSubmatch(content, -1)
-	imgs := make([]ImgSrc, len(matches))
-	for i, match := range matches {
-		var size, width, height uint64
-		path := match[2]
-		data := match[1] + match[3]
-		dataMatches := dataReg.FindAllStringSubmatch(data, -1)
-		for _, dm := range dataMatches {
-			switch dm[1] {
-			case "size":
-				size, _ = strconv.ParseUint(dm[2], 10, 32)
-			case "width":
-				width, _ = strconv.ParseUint(dm[2], 10, 16)
-			case "height":
-				height, _ = strconv.ParseUint(dm[2], 10, 16)
-			}
-		}
-		imgs[i] = ImgSrc{
-			Path:   path,
-			Size:   uint32(size),
-			Width:  uint16(width),
-			Height: uint16(height),
-		}
-	}
-	return imgs
 }
--- go.mod
+++ go.mod
@@ -45,8 +45,8 @@ require (
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/yosssi/ace v0.0.5 // indirect
 	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
-	golang.org/x/net v0.0.0-20220325170049-de3da57026de // indirect
-	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+	golang.org/x/net v0.0.0-20220403103023-749bd193bc2b // indirect
+	golang.org/x/sys v0.0.0-20220403020550-483a9cbc67c0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
