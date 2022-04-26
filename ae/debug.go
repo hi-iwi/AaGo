d6bd14f5dd46435cd8b7cc4726483ef71a67c4b5
Iwi <Aario@luexu.com>
2022-04-27 05:29:35 +0800

    calller()

--- ae/debug.go
+++ ae/debug.go
@@ -9,8 +9,17 @@ import (
 func Caller(skip int) string {
 	skip++ // 跳出Caller当前函数
 	pc, file, line, _ := runtime.Caller(skip)
-	a := strings.LastIndexByte(file, '/') + 1 // 文件名
-	pcs := runtime.FuncForPC(pc).Name()       // 函数名
+	var f string
+	seps := strings.Split(file, "/")
+	l := len(seps)
+	if l == 1 {
+		f = seps[0]
+	} else if l == 2 {
+		f = seps[l-2] + "/" + seps[l-1]
+	} else if l > 2 {
+		f = seps[l-3] + "/" + seps[l-2] + "/" + seps[l-1]
+	}
+	pcs := runtime.FuncForPC(pc).Name() // 函数名
 	pi := strings.LastIndexByte(pcs, '.') + 1
-	return "[" + file[a:] + ":" + strconv.Itoa(line) + " " + pcs[pi:] + "]"
+	return "[" + f + ":" + strconv.Itoa(line) + " " + pcs[pi:] + "]"
 }
