2529e23da0a47fb1701328d27c62473c09453d21
Iwi <Aario@luexu.com>
2022-05-05 16:00:18 +0800

    aorm.SafeIncr

--- aorm/aorm.go
+++ aorm/aorm.go
@@ -1,6 +1,44 @@
 package aorm
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
+
+func SafeIncr(field string, n int, max string) string {
+	if n == 0 {
+		return field + "=field"
+	}
+	s := field + "=CASE"
+	var ns string
+	if n > 0 {
+		ns = field + "+" + strconv.FormatUint(uint64(n), 10)
+		s += " WHEN " + ns + ">" + max + " THEN " + max
+	} else {
+		ns = field + "-" + strconv.FormatUint(uint64(-n), 10)
+		s += " WHEN " + ns + "<0 THEN 0"
+	}
+	s += " ELSE " + ns + " END"
+	return s
+}
+
+func SafeIncrUint8(field string, n int) string {
+	return SafeIncr(field, n, "255")
+}
+
+func SafeIncrUint16(field string, n int) string {
+	return SafeIncr(field, n, "65535")
+}
+func SafeIncrUint24(field string, n int) string {
+	return SafeIncr(field, n, "16777215")
+}
+
+func SafeIncrUint(field string, n int) string {
+	return SafeIncr(field, n, "4294967295")
+}
+func SafeIncrUint64(field string, n int) string {
+	return SafeIncr(field, n, "18446744073709551615")
+}
 
 func toMySqlFieldName(k string) string {
 	fields := strings.Split(k, ".")
--- go.mod
+++ go.mod
@@ -47,7 +47,7 @@ require (
 	github.com/yosssi/ace v0.0.5 // indirect
 	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
-	golang.org/x/sys v0.0.0-20220429233432-b5fbb4746d32 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
