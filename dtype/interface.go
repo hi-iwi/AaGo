59eb4b70a93c2067dd3380b76eae11e1b35ae06a
Iwi <Aario@luexu.com>
2022-05-15 04:22:53 +0800

    ae.NotFound

---
+++ dtype/interface.go
@@ -0,0 +1,30 @@
+package dtype
+
+func XUint64s(v []uint64) []interface{} {
+	args := make([]interface{}, len(v))
+	for i, x := range v {
+		args[i] = x
+	}
+	return args
+}
+func XUints(v []int64) []interface{} {
+	args := make([]interface{}, len(v))
+	for i, x := range v {
+		args[i] = x
+	}
+	return args
+}
+func XInt64s(v []int64) []interface{} {
+	args := make([]interface{}, len(v))
+	for i, x := range v {
+		args[i] = x
+	}
+	return args
+}
+func XInts(v []int64) []interface{} {
+	args := make([]interface{}, len(v))
+	for i, x := range v {
+		args[i] = x
+	}
+	return args
+}
