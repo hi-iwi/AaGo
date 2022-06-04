7e8145ec10ce5a977288d3e9c557c102d8fbfc43
Iwi <Aario@luexu.com>
2022-06-04 20:03:59 +0800

    optimize redis table seperator

--- acache/redis.go
+++ acache/redis.go
@@ -9,14 +9,6 @@ import (
 	"time"
 )
 
-func weekIndex() int {
-	i := time.Now().Day() / 7 // 0 1 2 3
-	if i > 3 {
-		i = 3 // 当月29号之后都并入上一周期
-	}
-	return i
-}
-
 // 一般用于 SUnion
 func Uint64s(vs []string, err error) ([]uint64, *ae.Error) {
 	if err != nil {
@@ -116,21 +108,22 @@ func HMIncrIds(ctx context.Context, rdb *redis.Client, expires time.Duration, k 
 }
 
 // 使当前时段的放到最后
-func BatchKeys(n int, final int, ignoreCurrent bool, f func(int) string) []string {
+func BatchKeys(n uint8, final uint8, ignoreCurrent bool, f func(uint8) string) []string {
 	l := final
 	if ignoreCurrent {
 		l--
 	}
 	ks := make([]string, l)
-	j := 0
+	var i, j uint8
 	// 当前时段一定要在最后
 	if n+1 < final {
-		for i := n + 1; i < final; i++ {
+		for i = n + 1; i < final; i++ {
 			ks[j] = f(i)
 			j++
 		}
 	}
-	for i := 0; i < n; i++ {
+
+	for i = 0; i < n; i++ {
 		ks[j] = f(i)
 		j++
 	}
@@ -144,7 +137,7 @@ func BatchKeys(n int, final int, ignoreCurrent bool, f func(int) string) []strin
 }
 
 // final 不包括
-func Key(n int, final int, prev bool, f func(int) string) string {
+func Key(n uint8, final uint8, prev bool, f func(uint8) string) string {
 	if prev {
 		n--
 	}
@@ -153,14 +146,50 @@ func Key(n int, final int, prev bool, f func(int) string) string {
 	}
 	return f(n)
 }
-func HourlyKey(prev bool, f func(int) string) string {
-	return Key(time.Now().Hour(), 24, prev, f)
+
+func hourIdx(interval uint8) (uint8, uint8) {
+	max := uint8(24) // 一天24小时，因数：1，2，3，4，6，8，12，24
+	if interval > (max/2) || interval == 0 {
+		return 1, 1
+	}
+	if interval > max {
+		interval = max
+	}
+	final := max / interval
+	n := uint8(time.Now().Hour()) % final
+	return n, final
 }
 
-func WeeklyKey(prev bool, f func(int) string) string { return Key(weekIndex(), 4, prev, f) }
-func HourlyKeys(ignoreCurrent bool, f func(int) string) []string {
-	return BatchKeys(time.Now().Hour(), 24, ignoreCurrent, f)
+// @param interval uint8  一天内，每interval小时一张表，最大不能超过24，且被24整除，1，2，3，4，6，8，12，24
+func HourlyKey(interval uint8, prev bool, f func(uint8) string) string {
+	n, final := hourIdx(interval)
+	return Key(n, final, prev, f)
 }
-func WeeklyKeys(ignoreCurrent bool, f func(int) string) []string {
-	return BatchKeys(weekIndex(), 4, ignoreCurrent, f)
+func HourlyKeys(interval uint8, ignoreCurrent bool, f func(uint8) string) []string {
+	n, final := hourIdx(interval)
+	return BatchKeys(n, final, ignoreCurrent, f)
+}
+
+func dayIdx(interval uint8) (uint8, uint8) {
+	max := uint8(30) // 30 因数：1 2 3 5 6 10 15 30  因数越多，能够拆分的可能性越多，所以选择每个月30天
+	if interval > (max/2) || interval == 0 {
+		return 1, 1
+	}
+	if interval > max {
+		interval = max
+	}
+	final := max / interval
+	n := uint8(time.Now().Day()) % final
+	return n, final
+}
+
+// @param interval uint8 一个月内，每interval天一张表
+func DailyKey(interval uint8, prev bool, f func(uint8) string) string {
+	n, final := dayIdx(interval)
+	return Key(n, final, prev, f)
+}
+
+func DailyKeys(interval uint8, ignoreCurrent bool, f func(uint8) string) []string {
+	n, final := dayIdx(interval)
+	return BatchKeys(n, final, ignoreCurrent, f)
 }
