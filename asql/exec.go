0b371fdc51e928173791d0713e90ce03f23d8d6f
Iwi <Aario@luexu.com>
2022-08-16 08:45:52 +0800

    fixed stmt close caused bad connection

--- asql/exec.go
+++ asql/exec.go
@@ -53,31 +53,39 @@ func (d *DB) Prepare(ctx context.Context, query string) (*sql.Stmt, *ae.Error) {
 	return stmt, ae.NewSqlError(err)
 }
 
-func (d *DB) Exec(ctx context.Context, query string, args ...interface{}) (sql.Result, *ae.Error) {
+/*
+  stmt close 必须要等到相关都执行完（包括  res.LastInsertId()  ,  row.Scan()
+*/
+func (d *DB) Exec(ctx context.Context, query string, args ...interface{}) (*sql.Stmt, sql.Result, *ae.Error) {
 	stmt, e := d.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
 	res, err := stmt.ExecContext(ctx, args...)
-	return res, ae.NewSqlError(err)
+	if err != nil {
+		stmt.Close()
+		return nil, res, ae.NewSqlError(err)
+	}
+	return stmt, res, nil
 }
 
 func (d *DB) Insert(ctx context.Context, query string, args ...interface{}) (uint, *ae.Error) {
-	res, e := d.Exec(ctx, query, args...)
+	stmt, res, e := d.Exec(ctx, query, args...)
 	if e != nil {
 		return 0, e
 	}
+	defer stmt.Close()
 	// 由于事务是先执行，后回滚或提交，所以可以先获取插入的ID，后commit()
 	id, err := res.LastInsertId()
 	return uint(id), ae.NewSqlError(err)
 }
 
 func (d *DB) Update(ctx context.Context, query string, args ...interface{}) (int64, *ae.Error) {
-	res, e := d.Exec(ctx, query, args...)
+	stmt, res, e := d.Exec(ctx, query, args...)
 	if e != nil {
 		return 0, e
 	}
+	defer stmt.Close()
 	// 由于事务是先执行，后回滚或提交，所以可以先获取更新结果，后commit()
 	id, err := res.RowsAffected()
 	return id, ae.NewSqlError(err)
@@ -91,68 +99,78 @@ func (d *DB) Update(ctx context.Context, query string, args ...interface{}) (int
 		stmt.QueryRowContext(ctx, i).&Scan()
 	}
 */
-func (d *DB) BatchQueryRow(ctx context.Context, query string, margs ...[]interface{}) ([]*sql.Row, *ae.Error) {
+func (d *DB) BatchQueryRow(ctx context.Context, query string, margs ...[]interface{}) (*sql.Stmt, []*sql.Row, *ae.Error) {
 	stmt, e := d.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
 	rows := make([]*sql.Row, len(margs))
 	for i, args := range margs {
 		rows[i] = stmt.QueryRowContext(ctx, args...)
 	}
-	return rows, nil
+	return stmt, rows, nil
 }
 
-func (d *DB) QueryRow(ctx context.Context, query string, args ...interface{}) (*sql.Row, *ae.Error) {
+func (d *DB) QueryRow(ctx context.Context, query string, args ...interface{}) (*sql.Stmt, *sql.Row, *ae.Error) {
 	stmt, e := d.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
 	row := stmt.QueryRowContext(ctx, args...)
-	return row, ae.NewSqlError(row.Err())
+	if row.Err() != nil {
+		stmt.Close()
+		return nil, nil, ae.NewSqlError(row.Err())
+	}
+	return stmt, row, nil
 }
 
 func (d *DB) ScanArgs(ctx context.Context, query string, args []interface{}, dest ...interface{}) *ae.Error {
-	row, e := d.QueryRow(ctx, query, args...)
+	stmt, row, e := d.QueryRow(ctx, query, args...)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 func (d *DB) ScanRow(ctx context.Context, query string, dest ...interface{}) *ae.Error {
-	row, e := d.QueryRow(ctx, query)
+	stmt, row, e := d.QueryRow(ctx, query)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 
 func (d *DB) Scan(ctx context.Context, query string, id uint64, dest ...interface{}) *ae.Error {
-	row, e := d.QueryRow(ctx, query, id)
+	stmt, row, e := d.QueryRow(ctx, query, id)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 func (d *DB) ScanX(ctx context.Context, query string, id string, dest ...interface{}) *ae.Error {
-	row, e := d.QueryRow(ctx, query, id)
+	stmt, row, e := d.QueryRow(ctx, query, id)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 
 // do not forget to close *sql.Rows
 // 不要忘了关闭 rows
 // 只有 QueryRow 找不到才会返回 ae.NotFound；Query 即使不存在，也是 nil
-func (d *DB) Query(ctx context.Context, query string, args ...interface{}) (*sql.Rows, *ae.Error) {
+func (d *DB) Query(ctx context.Context, query string, args ...interface{}) (*sql.Stmt, *sql.Rows, *ae.Error) {
 	stmt, e := d.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
+
 	rows, err := stmt.QueryContext(ctx, args...)
-	return rows, ae.NewSqlError(err)
+	if err != nil {
+		stmt.Close()
+		return nil, nil, ae.NewSqlError(err)
+	}
+	return stmt, rows, nil
 }
--- asql/exec_tx.go
+++ asql/exec_tx.go
@@ -41,31 +41,36 @@ func (t *Tx) Prepare(ctx context.Context, query string) (*sql.Stmt, *ae.Error) {
 	return stmt, ae.NewSqlError(err)
 }
 
-func (t *Tx) Exec(ctx context.Context, query string, args ...interface{}) (sql.Result, *ae.Error) {
+func (t *Tx) Exec(ctx context.Context, query string, args ...interface{}) (*sql.Stmt, sql.Result, *ae.Error) {
 	stmt, e := t.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
 	res, err := stmt.ExecContext(ctx, args...)
-	return res, ae.NewSqlError(err)
+	if err != nil {
+		stmt.Close()
+		return nil, res, ae.NewSqlError(err)
+	}
+	return stmt, res, nil
 }
 
 func (t *Tx) Insert(ctx context.Context, query string, args ...interface{}) (uint, *ae.Error) {
-	res, e := t.Exec(ctx, query, args...)
+	stmt, res, e := t.Exec(ctx, query, args...)
 	if e != nil {
 		return 0, e
 	}
+	defer stmt.Close()
 	// 由于事务是先执行，后回滚或提交，所以可以先获取插入的ID，后commit()
 	id, err := res.LastInsertId()
 	return uint(id), ae.NewSqlError(err)
 }
 
 func (t *Tx) Update(ctx context.Context, query string, args ...interface{}) (int64, *ae.Error) {
-	res, e := t.Exec(ctx, query, args...)
+	stmt, res, e := t.Exec(ctx, query, args...)
 	if e != nil {
 		return 0, e
 	}
+	defer stmt.Close()
 	// 由于事务是先执行，后回滚或提交，所以可以先获取更新结果，后commit()
 	id, err := res.RowsAffected()
 	return id, ae.NewSqlError(err)
@@ -79,69 +84,78 @@ func (t *Tx) Update(ctx context.Context, query string, args ...interface{}) (int
 		stmt.QueryRowContext(ctx, i).&Scan()
 	}
 */
-func (t *Tx) BatchQueryRow(ctx context.Context, query string, margs ...[]interface{}) ([]*sql.Row, *ae.Error) {
+func (t *Tx) BatchQueryRow(ctx context.Context, query string, margs ...[]interface{}) (*sql.Stmt, []*sql.Row, *ae.Error) {
 	stmt, e := t.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
 	rows := make([]*sql.Row, len(margs))
 	for i, args := range margs {
 		rows[i] = stmt.QueryRowContext(ctx, args...)
 	}
-	return rows, nil
+	return stmt, rows, nil
 }
 
-func (t *Tx) QueryRow(ctx context.Context, query string, args ...interface{}) (*sql.Row, *ae.Error) {
+func (t *Tx) QueryRow(ctx context.Context, query string, args ...interface{}) (*sql.Stmt, *sql.Row, *ae.Error) {
 	stmt, e := t.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
 	row := stmt.QueryRowContext(ctx, args...)
-	return row, ae.NewSqlError(row.Err())
+	if row.Err() != nil {
+		stmt.Close()
+		return nil, nil, ae.NewSqlError(row.Err())
+	}
+	return stmt, row, nil
 }
 
 func (t *Tx) ScanArgs(ctx context.Context, query string, args []interface{}, dest ...interface{}) *ae.Error {
-	row, e := t.QueryRow(ctx, query, args...)
+	stmt, row, e := t.QueryRow(ctx, query, args...)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 
 func (t *Tx) ScanRow(ctx context.Context, query string, dest ...interface{}) *ae.Error {
-	row, e := t.QueryRow(ctx, query)
+	stmt, row, e := t.QueryRow(ctx, query)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 
 func (t *Tx) Scan(ctx context.Context, query string, id uint64, dest ...interface{}) *ae.Error {
-	row, e := t.QueryRow(ctx, query, id)
+	stmt, row, e := t.QueryRow(ctx, query, id)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 
 func (t *Tx) ScanX(ctx context.Context, query string, id string, dest ...interface{}) *ae.Error {
-	row, e := t.QueryRow(ctx, query, id)
+	stmt, row, e := t.QueryRow(ctx, query, id)
 	if e != nil {
 		return e
 	}
+	defer stmt.Close()
 	return ae.NewSqlError(row.Scan(dest...))
 }
 
 // do not forget to close *sql.Rows
 // 不要忘了关闭 rows
-func (t *Tx) Query(ctx context.Context, query string, args ...interface{}) (*sql.Rows, *ae.Error) {
+func (t *Tx) Query(ctx context.Context, query string, args ...interface{}) (*sql.Stmt, *sql.Rows, *ae.Error) {
 	stmt, e := t.Prepare(ctx, query)
 	if e != nil {
-		return nil, e
+		return stmt, nil, e
 	}
-	defer stmt.Close()
 	rows, err := stmt.QueryContext(ctx, args...)
-	return rows, ae.NewSqlError(err)
+	if err != nil {
+		stmt.Close()
+		return nil, nil, ae.NewSqlError(err)
+	}
+	return stmt, rows, nil
 }
