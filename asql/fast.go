9bf3c5608fa523532931219bf653fe9471696f6f
hi31182508@aliyun.com <root@localhost.localdomain>
2023-06-04 18:07:05 +0800

    asql.In

--- asql/fast.go
+++ asql/fast.go
@@ -1,5 +1,29 @@
 package asql
 
+import (
+	"github.com/hi-iwi/AaGo/atype"
+	"strconv"
+)
+
+func In(field string, ids []uint64) string {
+	if len(ids) == 0 {
+		return "1!=1"
+	}
+	if len(ids) == 1 {
+		return field + "=" + strconv.FormatUint(ids[0], 10)
+	}
+	return field + "=" + atype.JoinUint64(ids, ',')
+}
+func InUint(field string, ids []uint) string {
+	if len(ids) == 0 {
+		return "1!=1"
+	}
+	if len(ids) == 1 {
+		return field + "=" + strconv.FormatUint(uint64(ids[0]), 10)
+	}
+	return field + "=" + atype.JoinUint(ids, ',')
+}
+
 /*
   组合sql语句，用于修改符合valid条件的字段
   @return ["a=?","b=?"], [$a,$b]
