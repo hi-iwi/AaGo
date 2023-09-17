1f179e15ead86e2beebddf30313eec6bd5b430bb
hi31182508@aliyun.com <root@localhost.localdomain>
2023-09-17 17:45:26 +0800

    refactor:atype.TelX

--- atype/sql_x.go
+++ atype/sql_x.go
@@ -4,7 +4,7 @@ import "context"
 
 type TelX string // varchar(13)
 
-func (x TelX) Decode(ctx context.Context, desense bool, decoder func(context.Context, string, bool) (string, bool)) string {
-	tel, _ := decoder(ctx, string(x), desense)
+func (x TelX) Decode(ctx context.Context, desense bool, decoder func(context.Context, TelX, bool) (string, bool)) string {
+	tel, _ := decoder(ctx, x, desense)
 	return tel
 }
