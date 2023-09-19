5f2d1d260ed42429761dbe9c40f24e2340061ef5
hi31182508@aliyun.com <root@localhost.localdomain>
2023-09-20 01:54:51 +0800

    refactor(CacheErrors): return error handled or not

--- com/resp_catch_error.go
+++ com/resp_catch_error.go
@@ -4,12 +4,12 @@ import (
 	"github.com/hi-iwi/AaGo/ae"
 )
 
-func (resp *RespStruct) CatchErrors(es ...*ae.Error) *ae.Error {
+func (resp *RespStruct) CatchErrors(es ...*ae.Error) (bool, *ae.Error) {
 	for i := 0; i < len(es); i++ {
 		if es[i] != nil {
 			resp.WriteSafeE(*es[i])
-			return es[i]
+			return true, es[i]
 		}
 	}
-	return nil
+	return false, nil
 }
