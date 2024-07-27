f2cdad3e2df78a9967ae866941f758356eff6e93
Aario <root@localhost.localdomain>
2024-07-28 04:39:10 +0800

    upgrade iris to 12.2.11

--- ae/alias.go
+++ ae/alias.go
@@ -7,22 +7,24 @@ var (
 	// @warn 自定义。444 空数组返回这个错误，表示不可以再进行下一页查询了
 	// 若是 code:204, data:[]  空数组，表示查询到了数据，但是数据过滤完了，可以尝试下一页查询
 	NoContent       = &Error{204, "No Content"}
+	BadRequest      = &Error{400, "Bad Request"}
 	Unauthorized    = &Error{401, "Unauthorized"}
 	PaymentRequired = &Error{402, "Payment Required"}
 	//403 Forbidden
 	//服务器已经理解请求，但是拒绝执行它。与401响应不同的是，身份验证并不能提供任何帮助，而且这个请求也不应该被重复提交。如果这不是一个 HEAD 请求，而且服务器希望能够讲清楚为何请求不能被执行，那么就应该在实体内描述拒绝的原因。当然服务器也可以返回一个404响应，假如它不希望让客户端获得任何信息。
 	Forbidden = &Error{403, "Forbidden"}
 	NotFound  = &Error{404, "Not Found"} // refer to redis.Nil, sql.ErrNoRows
+	//NotAcceptable = &Error{406, "Not Acceptable"} //
 	// 空数组返回 422
 	//409 Conflict
 	//由于和被请求的资源的当前状态之间存在冲突，请求无法完成。这个代码只允许用在这样的情况下才能被使用：用户被认为能够解决冲突，并且会重新提交新的请求。该响应应当包含足够的信息以便用户发现冲突的源头。
 	//冲突通常发生于对 PUT 请求的处理中。例如，在采用版本检查的环境下，某次 PUT 提交的对特定资源的修改请求所附带的版本信息与之前的某个（第三方）请求向冲突，那么此时服务器就应该返回一个409错误，告知用户请求无法完成。此时，响应实体中很可能会包含两个冲突版本之间的差异比较，以便用户重新提交归并以后的新版本。
 	Timeout = &Error{408, "Request Timeout"} // 被限流也是返回这个
 
-	Conflict           = &Error{409, "Conflict"}
-	Gone               = &Error{410, "Gone"} // 以前存在过，以后都不会再存在了，表示数据已经删除、过期、失效
-	PreconditionFailed = &Error{412, "Precondition Failed"}
-	BadMediaType       = &Error{415, "Unsupported Media Type"} // 上传的数据格式非法
+	Conflict = &Error{409, "Conflict"}
+	Gone     = &Error{410, "Gone"} // 以前存在过，以后都不会再存在了，表示数据已经删除、过期、失效
+	//PreconditionFailed = &Error{412, "Precondition Failed"}  这个与406类似
+	BadMediaType = &Error{415, "Unsupported Media Type"} // 上传的数据格式非法
 
 	// @warn 自定义。444 空数组返回这个错误，表示不可以再进行下一页查询了
 	// 若是 code:204, data:[]  空数组，表示查询到了数据，但是数据过滤完了，可以尝试下一页查询
