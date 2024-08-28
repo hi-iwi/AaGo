505e10e1bbdbf22f0adf3606501d43ad049c39ab
Aario <root@localhost.localdomain>
2024-08-29 02:50:21 +0800

    feat:atype.DocumentSrc

--- atype/dto_mime_document.go
+++ atype/dto_mime_document.go
@@ -10,5 +10,7 @@ type DocumentSrc struct {
 	Url      string         `json:"url"`
 	Filetype aenum.FileType `json:"filetype"` // aenum.Filetype.Int8()
 	Size     int            `json:"size"`     // atype.Uint24.Int8()
+	Checksum string         `json:"checksum"` // 图片、视频、音频会被压缩，checksum 无意义；这类文件不能被压缩
+	Info     string         `json:"info"`     // 冗余数据
 	Jsonkey  string         `json:"jsonkey"`  // 特殊约定字段
 }
