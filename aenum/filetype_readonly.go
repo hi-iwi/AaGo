127f9e0fa07a27c9e21392731e8926cbd1aa0963
Aario <root@localhost.localdomain>
2024-08-29 01:35:04 +0800

    feat:build filetype enum

--- aenum/filetype_readonly.go
+++ aenum/filetype_readonly.go
@@ -58,7 +58,7 @@ var VideoTypes = map[FileType][]string{
     Wav         : {".wav", "video/x-wav"},
     Webm        : {".webm", "video/webm"},
 }
-var TextTypes = map[FileType][]string{
+var DocumentTypes = map[FileType][]string{
     Doc         : {".doc", "application/msword"},
     Docx        : {".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
     Json        : {".json", "application/json"},
@@ -86,25 +86,25 @@ var ImageTypes = map[FileType][]string{
     Svg         : {".svg", "image/svg+xml"},
     Webp        : {".webp", "image/webp"},
 }
-func NewImageType(mime string) (FileType, bool) {return ParseFileType(mime, ImageTypes)}
 func NewAudioType(mime string) (FileType, bool) {return ParseFileType(mime, AudioTypes)}
 func NewVideoType(mime string) (FileType, bool) {return ParseFileType(mime, VideoTypes)}
-func NewTextType(mime string) (FileType, bool) {return ParseFileType(mime, TextTypes)}
+func NewDocumentType(mime string) (FileType, bool) {return ParseFileType(mime, DocumentTypes)}
 func NewCompressedType(mime string) (FileType, bool) {return ParseFileType(mime, CompressedTypes)}
+func NewImageType(mime string) (FileType, bool) {return ParseFileType(mime, ImageTypes)}
 func (t FileType) ContentType() string {
-    if d, ok := CompressedTypes[t]; ok {return d[1]}
-    if d, ok := ImageTypes[t]; ok {return d[1]}
     if d, ok := AudioTypes[t]; ok {return d[1]}
     if d, ok := VideoTypes[t]; ok {return d[1]}
-    if d, ok := TextTypes[t]; ok {return d[1]}
+    if d, ok := DocumentTypes[t]; ok {return d[1]}
+    if d, ok := CompressedTypes[t]; ok {return d[1]}
+    if d, ok := ImageTypes[t]; ok {return d[1]}
     return ""
 }
 func (t FileType) Ext() string {
-    if d, ok := ImageTypes[t]; ok {return d[0]}
     if d, ok := AudioTypes[t]; ok {return d[0]}
     if d, ok := VideoTypes[t]; ok {return d[0]}
-    if d, ok := TextTypes[t]; ok {return d[0]}
+    if d, ok := DocumentTypes[t]; ok {return d[0]}
     if d, ok := CompressedTypes[t]; ok {return d[0]}
+    if d, ok := ImageTypes[t]; ok {return d[0]}
     return ""
 }
 func (t FileType) Name() string {return strings.TrimPrefix(t.Ext(), ".")}
